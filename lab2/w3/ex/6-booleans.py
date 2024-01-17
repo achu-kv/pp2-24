ed06e3b726614c0894519dccd461f9eb8d6623ac
Alex <a_chumakov@kbtu.kz>
2024-01-17 14:39:19 +0600

    lab2

---
+++ lab2/w3/ex/6-booleans.py
@@ -0,0 +1,20 @@
+# task 1
+print(10 > 9)
+# outputs true
+
+# task 2
+print(10 == 9)
+# outputs false
+
+# task 3
+print(10 < 9)
+# outputs false
+
+# task 4
+print(bool("abc"))
+# outputs true 
+
+# task 5
+print(bool(0))
+# outputs false
+
